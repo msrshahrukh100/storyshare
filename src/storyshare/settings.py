a67d1e95c0c38cb9ece40e3c7793f6e55098035b
Shahrukh <mohammad.shahrukh@go-mmt.com>
2021-03-24 11:40:51 +0530

    Working on models

--- src/storyshare/settings.py
+++ src/storyshare/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!t8e43j2$&0lw&zs!$6-vu_b5!g_fvu3u54=%rf%5yl!@$mm^w'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
